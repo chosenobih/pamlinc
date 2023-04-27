d3747194d702aa0c71023280443ba7566701641a
chosenobih <obihchosen@yahoo.com>
2023-04-27 09:37:30 -0700

    modified the setup script

--- setup_script.sh
+++ setup_script.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
-#create new conda environment
-conda create -n pamlinc_env python=3.10.6
-conda activate pamlinc_env
-
 #Add conda channels
 conda config --add channels conda-forge 
 conda config --add channels defaults  
 conda config --add channels bioconda
 
 #install conda packages
-conda install star==2.7.10a -c bioconda
-conda install trimmomatic==0.35 -c bioconda
-conda install gffread==0.12.1 -c bioconda
-conda install subread==2.0.1 -c bioconda
-conda install samtools==1.9 -c bioconda
-conda install cufflinks==2.2.1 -c bioconda
-conda install stringtie==2.1.5 -c bioconda
-conda install picard==2.18.27 -c bioconda
+echo "installing packages required to run pamlinc"
+
+conda install star==2.7.10a -c bioconda -y
+conda install trimmomatic==0.35 -c bioconda -y
+conda install gffread==0.12.1 -c bioconda -y
+conda install subread==2.0.1 -c bioconda -y
+conda install samtools==1.9 -c bioconda -y
+conda install cufflinks==2.2.1 -c bioconda -y
+conda install stringtie==2.1.5 -c bioconda -y
+conda install picard==2.18.27 -c bioconda -y
+
+echo "installation done"
